90d2443c05819936e30923988a52dde1bbb64f59
Rajat Sharma <lunasunkaiser@gmail.com>
2018-11-10 02:37:26 +0530

    Added Readme

--- readme.md
+++ readme.md
@@ -1,4 +1,36 @@
-## mu2
+## ITMS
 
-_Basic Compositon of all the repos._
+_Inter Tabs Messaging System._
 
+#### Setup and Usage
+
+- Add itms worker script in your `service-worker.js`.
+
+`importScripts('https://firebasestorage.googleapis.com/v0/b/firebase-gluon.appspot.com/o/itms-worker.js?alt=media&token=f63e1284-bad7-4079-9242-5fc6daf9219f');`
+
+- Make sure you have included serviceworker in your script using,
+
+`navigator.serviceWorker.register('service-worker.js');`
+
+```javascript
+import Itms from 'itms';
+
+const itms = Itms();
+
+// Add Event Listener
+itms
+  .on('logout', e =>
+    app.setState({
+      login: false,
+    }),
+  )
+  .on('login', e =>
+    app.setState({
+      login: true,
+    }),
+  )
+  .deploy();
+
+//Emit Events
+itms.emit(this.state.login ? 'logout' : 'login');
+```
--- src/itms-worker.js
+++ src/itms-worker.js
@@ -1,16 +1,11 @@
 /*eslint-disable*/
 self.addEventListener('message', function(event) {
-  // ¶
   // Get all the connected clients and forward the message along.
-
   var promise = self.clients.matchAll().then(function(clientList) {
-    // ¶
     // event.source.id contains the ID of the sender of the message.
-
     var senderID = event.source.id;
 
     clientList.forEach(function(client) {
-      // ¶
       // Skip sending the message to the client that sent it.
 
       if (client.id === senderID) {
@@ -22,14 +17,12 @@ self.addEventListener('message', function(event) {
       });
     });
   });
-  // ¶
   // If event.waitUntil is defined, use it to extend the lifetime of the Service Worker.
 
   if (event.waitUntil) {
     event.waitUntil(promise);
   }
 });
-// ¶
 // Immediately claim any new clients. This is not needed to send messages, but makes for a better demo experience since the user does not need to refresh. A more complete example of this given in the immediate-claim recipe.
 
 self.addEventListener('activate', function(event) {
