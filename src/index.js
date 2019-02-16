ad90464296f9c176e02ac6015764692a3e2fd3fd
Rajat Sharma <lunasunkaiser@gmail.com>
2019-02-16 21:54:15 +0530

    Reusing Fns

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import either from './Main.purs';
 
-const runiffn = (maybeFn, ...args) =>
-  (typeof maybeFn === 'function'
-    ? either.Left.create(maybeFn)
-    : either.Right.create(maybeFn)) |> either.either(fn => fn(...args))(_ => _);
-
 export const iffn = maybeFn =>
   typeof maybeFn === 'function'
     ? either.Left.create(maybeFn)
     : either.Right.create(maybeFn);
 
-export const { bimap } = either.bifunctorEither;
-
 export const run = (...args) => either.either(fn => fn(...args))(_ => _);
+
+const runiffn = (maybeFn, ...args) => iffn(maybeFn) |> run(...args);
+
+export const { bimap } = either.bifunctorEither;
 
 export default runiffn;
